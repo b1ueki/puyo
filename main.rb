f66778ee10db5d16a2373d7f1721482467b1a31d
b1ueki <xxaxis.kixx@gmail.com>
2016-12-10 16:44:24 +0900

    IMG_WIDTH,IMG_HEIGHTに変更

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ Window.width   = 640
 Window.height  = 480
 
 game_objects = Array.new
-game_objects.push Puyo.new(($RIGHT - $LEFT) / 2,$TOP,Image.new(30, 30, [255, 255, 255]))
+game_objects.push Puyo.new(($RIGHT - $LEFT) / 2,$TOP,Image.new($IMG_WIDTH, $IMG_HEIGHT, [255, 255, 255]))
 
 Window.loop do
 
