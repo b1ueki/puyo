c1ee5cc1fa2a0451f125eb26066b6a1810616bbc
d1230 <d1230>
2016-12-10 17:22:02 +0900

    タイトルを追加

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 require 'dxruby'
 require_relative 'puyo'
@@ -7,12 +7,18 @@ Window.caption = "PuyoPuyo"
 Window.width   = 640
 Window.height  = 480
 
+font = Font.new(32)
+
 game_objects = Array.new
 game_objects.push Puyo.new(($RIGHT - $LEFT) / 2,$TOP,Image.new($IMG_WIDTH, $IMG_HEIGHT, [255, 255, 255]))
 
+title_flg = true
 Window.loop do
-
-  Sprite.update(game_objects)
-  Sprite.draw(game_objects)
-
+  title_flg = false if Input.keyPush?(K_SPACE)
+  if title_flg
+    Window.draw_font(100, 100, "スペースキーを押してください。", font)
+  else
+    Sprite.update(game_objects)
+    Sprite.draw(game_objects)
+  end
 end
