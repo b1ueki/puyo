d7c22a4d61330e9a5f5f38a1e506aaa55eb73c80
b1ueki <xxaxis.kixx@gmail.com>
2016-12-11 10:29:45 +0900

    controllable_puyo_groupとpuyoを変更
    BOTTOMに到達したときにスッてならないようにした
    回転と移動もできないようにした

--- controllable_puyo_group.rb
+++ controllable_puyo_group.rb
@@ -6,41 +6,56 @@ class ControllablePuyoGroup < Sprite
     @puyos = [Puyo.new(x,y,img1),Puyo.new(x,y + $IMG_HEIGHT,img2)]
   end
   def update
-    @puyos[0].update
-    @puyos[1].update
-    @puyos[0].x += $IMG_WIDTH if Input.key_push?(K_RIGHT) && $RIGHT - $IMG_WIDTH >= @puyos[0].x
-    @puyos[0].x -= $IMG_WIDTH if Input.key_push?(K_LEFT) && $LEFT < @puyos[0].x
-    @puyos[1].x += $IMG_WIDTH if Input.key_push?(K_RIGHT) && $RIGHT - $IMG_WIDTH >= @puyos[1].x
-    @puyos[1].x -= $IMG_WIDTH if Input.key_push?(K_LEFT) && $LEFT < @puyos[1].x
-    if Input.key_push?(K_X)
+    #落下
       if @puyos[0].y < @puyos[1].y
-        @puyos[0].x += $IMG_WIDTH
-        @puyos[0].y += $IMG_HEIGHT
+        @puyos[0].update_top
+        @puyos[1].update
       elsif @puyos[0].y > @puyos[1].y
-        @puyos[0].x -= $IMG_WIDTH
-        @puyos[0].y -= $IMG_HEIGHT
-      elsif @puyos[0].x > @puyos[1].x
-        @puyos[0].x -= $IMG_WIDTH
-        @puyos[0].y += $IMG_HEIGHT
+        @puyos[0].update
+        @puyos[1].update_top
       else
-        @puyos[0].x += $IMG_WIDTH
-        @puyos[0].y -= $IMG_HEIGHT
+        @puyos[0].update
+        @puyos[1].update
       end
-    elsif Input.key_push?(K_Z)
-      if @puyos[0].y < @puyos[1].y
-        @puyos[0].x -= $IMG_WIDTH
-        @puyos[0].y += $IMG_HEIGHT
-      elsif @puyos[0].y > @puyos[1].y
-        @puyos[0].x += $IMG_WIDTH
-        @puyos[0].y -= $IMG_HEIGHT
-      elsif @puyos[0].x > @puyos[1].x
-        @puyos[0].x -= $IMG_WIDTH
-        @puyos[0].y -= $IMG_HEIGHT
-      else
-        @puyos[0].x += $IMG_WIDTH
-        @puyos[0].y += $IMG_HEIGHT
-      end
-    end
+
+    if @puyos[0].y < $BOTTOM - $IMG_HEIGHT && @puyos[1].y < $BOTTOM - $IMG_HEIGHT 
+      #平行移動
+      @puyos[0].x += $IMG_WIDTH if Input.key_push?(K_RIGHT) && $RIGHT - $IMG_WIDTH > @puyos[0].x
+      @puyos[0].x -= $IMG_WIDTH if Input.key_push?(K_LEFT) && $LEFT < @puyos[0].x
+      @puyos[1].x += $IMG_WIDTH if Input.key_push?(K_RIGHT) && $RIGHT - $IMG_WIDTH > @puyos[1].x
+      @puyos[1].x -= $IMG_WIDTH if Input.key_push?(K_LEFT) && $LEFT < @puyos[1].x
+
+     #回転
+     if Input.key_push?(K_X)
+       if @puyos[0].y < @puyos[1].y
+         @puyos[0].x += $IMG_WIDTH
+         @puyos[0].y += $IMG_HEIGHT
+       elsif @puyos[0].y > @puyos[1].y
+         @puyos[0].x -= $IMG_WIDTH
+         @puyos[0].y -= $IMG_HEIGHT
+       elsif @puyos[0].x > @puyos[1].x
+         @puyos[0].x -= $IMG_WIDTH
+         @puyos[0].y += $IMG_HEIGHT
+       else
+         @puyos[0].x += $IMG_WIDTH
+         @puyos[0].y -= $IMG_HEIGHT
+       end
+     elsif Input.key_push?(K_Z)
+       if @puyos[0].y < @puyos[1].y
+         @puyos[0].x -= $IMG_WIDTH
+         @puyos[0].y += $IMG_HEIGHT
+       elsif @puyos[0].y > @puyos[1].y
+         @puyos[0].x += $IMG_WIDTH
+         @puyos[0].y -= $IMG_HEIGHT
+       elsif @puyos[0].x > @puyos[1].x
+         @puyos[0].x -= $IMG_WIDTH
+         @puyos[0].y -= $IMG_HEIGHT
+       else
+         @puyos[0].x += $IMG_WIDTH
+         @puyos[0].y += $IMG_HEIGHT
+       end
+     end
+   end
   end
   def draw
     @puyos[0].draw
